c1d099eb8678c37ce6adb4d341ec3728346edf5a
jwendyr <jwendyr@yahoo.com>
2016-11-18 00:32:48 -0600

    Create RankSortMapper.java

---
+++ src/main/java/rank/RankSortMapper.java
@@ -0,0 +1,26 @@
+package mapreduce.pagerank.rank;
+
+import org.apache.hadoop.io.FloatWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+public class RankSortMapper extends Mapper<LongWritable, Text, FloatWritable, Text> {
+
+    @Override
+    public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+    	// Get the page and split by tabs
+      String valueStr = value.toString();
+    	String[] sections = valueStr.split("\\t");
+    	
+    	// Get the rank and the current page (ignoring the pages at the end)
+    	float rank = Float.parseFloat(sections[1]);
+    	String page = sections[0];
+    	
+    	// Output rank first
+    	context.write(new FloatWritable(rank), new Text(page));
+    }
+    
+}
